2382b399ea05ae772a167017438ebfb2bea5f791
Claudio Campos <claudio@claudiocampos.com>
2016-01-10 17:46:37 -0500

    Completed Pascal's Triangle

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 (function () {
     var TriangleController = function ($scope) {
         var displayTriangle,
-            displayNumberOfRows;
+            getRowsCollection;
 
-        displayNumberOfRows = function () {
-            if ($scope.rowsInput) {
-                $scope.rows = $scope.rowsInput;
-            }
-        }
+        /* Function requested by ADP */
         displayTriangle = function () {
+            var rowsCollection = [],
+                thisArray = [],
+                previousArray = [];
 
+            for (var i = 0; i < $scope.rowsInput; i = i + 1) {
+                for (var j = 0; j < i + 1; j = j + 1) {
+                    if (j === 0) {
+                        thisArray = [];
+                        thisArray.push(1);
+                    } else if (j === 1) {
+                        thisArray = [];
+                        thisArray.push(1);
+                        thisArray.push(1);
+                    } else if (j > 1) {
+                        previousArray = thisArray;
+                        thisArray = [];
+                        for (var z = 0; z < previousArray.length; z = z + 1) {
+                            if (previousArray[z + 1]) {
+                                thisArray.push(previousArray[z] + previousArray[z + 1]);
+                            }
+                        }
+                        thisArray.unshift(1);
+                        thisArray.push(1);
+                        previousArray = thisArray;
+                    } else {
+                        return undefined;
+                    }
+                }
+                rowsCollection.push(thisArray);
+            }
+
+            $scope.rowsCollection = rowsCollection;
         }
-        $scope.hello = 'Hola';
+
         $scope.displayTriangle = displayTriangle;
-        $scope.displayNumberOfRows = displayNumberOfRows;
+        $scope.getRowsCollection = getRowsCollection;
     }
 
     angular.module('PascalTriangleApp', [])
